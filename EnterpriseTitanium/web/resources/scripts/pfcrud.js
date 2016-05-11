73e93fec78b461dc2a3a539ff59310d93b94b667
Julio Cortorreal <ing.julioccr@gmail.com>
2016-05-11 17:44:46 -0400

    Actulizacion css idioma evento español

--- EnterpriseTitanium/web/resources/scripts/pfcrud.js
+++ EnterpriseTitanium/web/resources/scripts/pfcrud.js
@@ -12,3 +12,26 @@ function fixPFMDialogs() {
         jQuery("body > div[id*='" + pageId + "'][class*='ui-popup']").appendTo("#" + pageId);
     });
 }
+
+ PrimeFaces.locales['es'] = {
+        closeText: 'Cerrar',
+        prevText: 'Atras',
+        nextText: 'Siguiente',
+        currentText: 'Fecha Actual',
+        monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio',
+            'Julio','Agosto','Septiempre','Octubre','Noviembre','Dicciembre'],
+        monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun',
+            'Jul','Ago','Sept','Oct','Nov','Dic'],
+        dayNames: ['Lunes','Martes','Miercoles','Jueves','Viernes','Sabado','Domingo'],
+        dayNamesShort: ['Dom','Lun','Mat','Mierc','Juev','Viern','Sab'],
+        dayNamesMin: ['Dm','Ln','Mt','Mc','Jv','Vn','Sb'],
+        weekHeader: 'Sem',
+        firstDay: 1,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: '',
+        month: 'Mes',
+        week: 'Semana',
+        day: 'Dia',
+        allDayText : 'Todo el Dia'
+    };
